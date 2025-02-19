b5f4f7c89798d6ff770ff39db9b1a8925765e51c
stasy-af <afoninanasta299@gmail.com>
2025-02-19 18:32:30 +0300

    update

--- untitled3/cmake-build-debug/cmake_install.cmake
+++ untitled3/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/настя/CLionProjects/untitled3
+# Install script for directory: C:/Users/настя/Project_uni/project_huffman/untitled3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/настя/CLionProjects/untitled3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/настя/Project_uni/project_huffman/untitled3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
